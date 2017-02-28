7e68d86d5dd0da86f04cb4d780d4b6b7a48536f6
Julien SENON <julien.senon@gmail.com>
2017-02-28 12:00:17 +0100

    json ecode decode and query

--- src/mycartravel.go
+++ src/mycartravel.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	//"io/ioutil"
+	"net/http"
+	"os"
 )
 
+// TO DO
+// How to ask google map
+// How to retrieve toll
+// Build Web Server
+// Fisrt Version with API Call
+// Seconf Version with google map go plugin
 // Define a struct for api distance matrix
+
 type Town struct {
 	TownFrom string
 	TownTo   string
@@ -17,16 +28,44 @@ type GazPrice struct {
 	Devise  string
 }
 
-// Define a struct for api distance matrix answer
-type TripAnswer struct {
-	Destination string
-	Origin      string
-	answer      map[string]map[string]map[string]map[string]float64
-	status      string
+// Define My Query
+var distanceMatrixAPI = &apiConfig{
+	host:            "https://maps.googleapis.com",
+	path:            "/maps/api/distancematrix/json",
+	acceptsClientID: true,
+}
+
+// Answer Structure coming from google API
+type DistanceMatrixResponse struct {
+
+	// OriginAddresses contains an array of addresses as returned by the API from your original request.
+	OriginAddresses []string `json:"origin_addresses"`
+	// DestinationAddresses contains an array of addresses as returned by the API from your original request.
+	DestinationAddresses []string `json:"destination_addresses"`
+	// Rows contains an array of elements.
+	Rows []DistanceMatrixElementsRow `json:"rows"`
+}
+
+// DistanceMatrixElementsRow is a row of distance elements.
+type DistanceMatrixElementsRow struct {
+	Elements []*DistanceMatrixElement `json:"elements"`
+}
+
+// Definition of DistanceElement
+// Define time.Duration and uncomment
+// Define distance and uncomment
+type DistanceMatrixElement struct {
+	Status string `json:"status"`
+	// Duration is the length of time it takes to travel this route.
+	//Duration time.Duration `json:"duration"`
+	// DurationInTraffic is the length of time it takes to travel this route considering traffic.
+	//DurationInTraffic time.Duration `json:"duration_in_traffic"`
+	// Distance is the total distance of this route.
+	//Distance Distance `json:"distance"`
 }
 
 /*
-https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=YOURAPIKEY
+https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Toulouse&destinations=Nantes&key=YOURAPIKEY
 
 {
    "destination_addresses" : [ "New York, NY, USA" ],
@@ -52,19 +91,24 @@ https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=
 }
 */
 
-// TO DO
-// How to ask google map
-// How to retrieve info and store it
-// How to retrieve toll
-// Build Web Server
-
 func main() {
-	env := []Town{
-		{"Toulouse", "Toulouse"},
+
+	// Initialization?
+	var mapsanswer DistanceMatrixResponse
+
+	// Export your API Key
+	apiKey := os.Getenv("GOOGLE_APIKEY")
+
+	fmt.Println("your key", apiKey)
+	// Launch connexion to maps google
+	res, erro := http.Get("https://maps.googleapis.com/maps/api/distancematrix/json" + "?units=imperial&origins=Toulouse&destinations=Nantes" + "&key=" + apiKey)
+	if erro != nil {
+		panic(erro.Error())
 	}
-	fmt.Println("Enter Origin:")
-	fmt.Scanf("%s", &env[0].TownFrom)
-	fmt.Println("Enter Destination:")
-	fmt.Scanf("%s", &env[0].TownTo)
-	fmt.Println("Trip", env)
+	err := json.NewDecoder(res.Body).Decode(&mapsanswer)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(mapsanswer.OriginAddresses)
+	fmt.Println(mapsanswer.DestinationAddresses)
 }
