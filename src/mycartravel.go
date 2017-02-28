32d1f0ffda1c407c98732c291bba81694147b61d
Julien SENON <julien.senon@gmail.com>
2017-02-28 18:08:35 +0100

    Correct struct for distance and duration

--- src/mycartravel.go
+++ src/mycartravel.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	//"io/ioutil"
 	"net/http"
 	"os"
 )
@@ -51,29 +50,49 @@ type DistanceMatrixElement struct {
 	Status string `json:"status"`
 	// Duration is the length of time it takes to travel this route.
 	//Duration time.Duration `json:"duration"`
+	Duration Duration `json:"duration"`
 	// DurationInTraffic is the length of time it takes to travel this route considering traffic.
-	//DurationInTraffic time.Duration `json:"duration_in_traffic"`
+	//DurationInTraffic Duration `json:"duration_in_traffic"`
 	// Distance is the total distance of this route.
-	//Distance Distance `json:"distance"`
+	Distance Distance `json:"distance"`
+}
+
+type Distance struct {
+	// HumanReadable is the human friendly distance. This is rounded and in an appropriate unit for the
+	// request. The units can be overriden with a request parameter.
+	HumanReadable string `json:"text"`
+	// Meters is the numeric distance, always in meters. This is intended to be used only in
+	// algorithmic situations, e.g. sorting results by some user specified metric.
+	Meters int `json:"value"`
+}
+
+type Duration struct {
+	// Value indicates the duration, in seconds.
+	Value int64 `json:"value"`
+	// Text contains a human-readable representation of the duration.
+	Text string `json:"text"`
 }
 
 /*
 https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Toulouse&destinations=Nantes&key=YOURAPIKEY
 
+units=metric (default) returns distances in kilometers and meters.
+units=imperial returns distances in miles and feet.
+
 {
-   "destination_addresses" : [ "New York, NY, USA" ],
-   "origin_addresses" : [ "Washington, DC, USA" ],
+   "destination_addresses" : [ "Nantes, France" ],
+   "origin_addresses" : [ "Toulouse, France" ],
    "rows" : [
       {
          "elements" : [
             {
                "distance" : {
-                  "text" : "225 mi",
-                  "value" : 361722
+                  "text" : "363 mi",
+                  "value" : 584955
                },
                "duration" : {
-                  "text" : "3 hours 48 mins",
-                  "value" : 13672
+                  "text" : "5 hours 14 mins",
+                  "value" : 18846
                },
                "status" : "OK"
             }
@@ -82,27 +101,29 @@ https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=
    ],
    "status" : "OK"
 }
+
 */
 
 func main() {
 
-	// Initialization?
+	// Initialization
 	var mapsanswer DistanceMatrixResponse
-
 	// Export your API Key
 	apiKey := os.Getenv("GOOGLE_APIKEY")
-
 	fmt.Println("your key", apiKey)
 	// Launch connexion to maps google
-	res, erro := http.Get("https://maps.googleapis.com/maps/api/distancematrix/json" + "?units=imperial&origins=Toulouse&destinations=Nantes" + "&key=" + apiKey)
-	defer res.Body.Close()
+	res, erro := http.Get("https://maps.googleapis.com/maps/api/distancematrix/json" + "?units=metric&origins=Toulouse&destinations=Nantes" + "&key=" + apiKey)
 	if erro != nil {
-		panic(erro.Error())
+		// handle error
+		return
 	}
-	err := json.NewDecoder(res.Body).Decode(&mapsanswer)
-	if err != nil {
-		panic(err.Error())
+	defer res.Body.Close()
+	if erro := json.NewDecoder(res.Body).Decode(&mapsanswer); erro != nil {
+		return
 	}
-	fmt.Println(mapsanswer.OriginAddresses)
-	fmt.Println(mapsanswer.DestinationAddresses)
+	fmt.Println(mapsanswer.OriginAddresses[0])
+	fmt.Println(mapsanswer.DestinationAddresses[0])
+	fmt.Println(mapsanswer.Rows[0].Elements[0].Duration.Text)
+	fmt.Println(mapsanswer.Rows[0].Elements[0].Distance.HumanReadable)
+
 }
