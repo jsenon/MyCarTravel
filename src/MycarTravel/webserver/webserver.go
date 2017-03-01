ff03f383c3ae397a811363d63b90ff653cfc9dd7
Julien Senon <julien.senon@gmail.com>
2017-03-01 10:24:26 +0100

    HTML page forms

--- src/MycarTravel/webserver/webserver.go
+++ src/MycarTravel/webserver/webserver.go
@@ -14,12 +14,17 @@ func Mytravelcarweb(res http.ResponseWriter, req *http.Request) {
 		res,
 		`<DOCTYPE html>
 		<html>
-		  <head>
-		    <title>MyCarTravel</title>
-		  </head>
-		  <body>
-		    MycarTravel
-		  </body>
+		<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>MyCarTravel</title>
+		</head>
+		<body>
+		<form action="http://127.0.0.1:9000/mycartravel?hello=world&thread=123" method="post" enctype="application/x-www-form-urlencoded">
+		<input type="text" name="Origin" value="Toulouse"/>
+		<input type="text" name="Destination" value="Nantes"/>
+		<input type="submit"/>
+		</form>
+		</body>
 		</html>`,
 	)
 }
