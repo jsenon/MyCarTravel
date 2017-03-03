6def03d1c7b889b60555f000c71000cd2a8cee42
Julien Senon <julien.senon@gmail.com>
2017-03-03 15:32:18 +0100

    cleaning in main

--- src/MycarTravel/mycartravel.go
+++ src/MycarTravel/mycartravel.go
@@ -18,33 +18,6 @@ import (
 
 /*
 https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Toulouse&destinations=Nantes&key=YOURAPIKEY
-
-units=metric (default) returns distances in kilometers and meters.
-units=imperial returns distances in miles and feet.
-
-{
-   "destination_addresses" : [ "Nantes, France" ],
-   "origin_addresses" : [ "Toulouse, France" ],
-   "rows" : [
-      {
-         "elements" : [
-            {
-               "distance" : {
-                  "text" : "363 mi",
-                  "value" : 584955
-               },
-               "duration" : {
-                  "text" : "5 hours 14 mins",
-                  "value" : 18846
-               },
-               "status" : "OK"
-            }
-         ]
-      }
-   ],
-   "status" : "OK"
-}
-
 */
 
 func main() {
@@ -55,7 +28,6 @@ func main() {
 	http.HandleFunc("/error", webserver.Wrong)
 	// Test Web page from template
 	http.HandleFunc("/htmltemplate", webserver.Htmltemplate)
-
 	// Init WebServer
 	http.ListenAndServe(":9000", nil)
 
